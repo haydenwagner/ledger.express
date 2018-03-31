24c3e51e786ba445e7c2321164a83712fb435538
Hayden Wagner <haydenblainewagner@gmail.com>
2018-03-31 11:13:04 -0400

    Update entry component and add view/edit mode (future)

--- src/app/ledger/entry/entry.component.ts
+++ src/app/ledger/entry/entry.component.ts
@@ -1,11 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+
+import {EntryMode} from '../../enums/entry-mode.enum';
+import {Entry} from '../../models/entry.model';
 
 @Component({
-  selector: 'app-entry',
+  selector: '[app-entry]',
   templateUrl: './entry.component.html',
   styleUrls: ['./entry.component.html']
 })
 export class EntryComponent implements OnInit {
+  EntryMode: typeof EntryMode = EntryMode;
+  mode: EntryMode = EntryMode.view;
+
+  @Input() entry: Entry;
+  @Input() index: number;
+
   constructor() {}
 
   ngOnInit() {}
