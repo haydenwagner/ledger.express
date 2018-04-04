0cc7b8296e9e1824479f3d94b3ca3ea41b96c5ed
Hayden Wagner <haydenblainewagner@gmail.com>
2018-04-03 20:54:24 -0400

    Adds create user collection on sign in function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,35 @@ import { Component} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from './auth.service';
 
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+
 import {NgbModal, ModalDismissReasons, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
+import {Entry} from './models/entry.model';
+
+export interface User {uid: string; entries: Entry[]; }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
   email: string;
   password: string;
   authError: false;
   private loginModal: NgbModalRef;
+  private userCollection: AngularFirestoreCollection<User>;
 
-  constructor(public authService: AuthService, private modalService: NgbModal) {}
+
+  constructor(
+    public authService: AuthService,
+    private modalService: NgbModal,
+    private afs: AngularFirestore
+  ) {
+    this.userCollection = afs.collection('user');
+  }
 
   openDialog(content) {
     this.loginModal = this.modalService.open(content);
@@ -25,6 +40,7 @@ export class AppComponent {
     this.authService.signup(this.email, this.password)
       .then(res => {
         this.email = this.password = '';
+        this.makeUserCollection(res.uid);
         this.loginModal.close();
       })
       .catch(err => {
@@ -49,5 +65,15 @@ export class AppComponent {
 
   clearAuthError(): void {
     this.authError = false;
+  }
+
+  makeUserCollection(uid) {
+    this.userCollection.add({uid : uid, entries: []})
+      .then(res => {
+        console.log('user document added to collection');
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
   }
 }
