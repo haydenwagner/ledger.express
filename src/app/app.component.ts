b43d481c7366321cd80eb7e99937aad8b64f46fe
Hayden Wagner <haydenblainewagner@gmail.com>
2018-04-15 19:44:17 -0400

    Remove ngxui modules and elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component} from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { AuthService } from './auth.service';
 
 import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { LedgerModule} from './ledger/ledger.module';
 
-import {NgxUIModule, DialogService} from '@swimlane/ngx-ui';
 
 import {AngularFireModule} from 'angularfire2';
 import {AngularFireAuthModule} from 'angularfire2/auth';
 import {AngularFirestoreModule} from 'angularfire2/firestore';
 
 import { AuthService } from './auth.service';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -23,17 +22,15 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     LedgerModule,
-    NgxUIModule,
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    AngularFirestoreModule,
-    NgbModule.forRoot()
+    AngularFirestoreModule
   ],
   providers: [
     AuthService,
-    DialogService
   ],
   bootstrap: [AppComponent]
 })
